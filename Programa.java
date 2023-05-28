14ad0b9f4ab6f35e7d649cadcec3e919bf7be637
Tamires <rodriguestamires510@gmail.com>
2023-05-28 13:16:27 -0300

    feat: separando classes proprias e validação de genero

--- Programa.java
+++ Programa.java
@@ -4,45 +4,27 @@ public class Programa {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Informe a idade do cliente: ");
-    int idade = scanner.nextInt();
+    ValidacaoIdadeHomem validacaoIdadeHomem = new ValidacaoIdadeHomem();
+		ValidacaoIdadeMulher validacaoIdadeMulher = new ValidacaoIdadeMulher();
+		ValidacaoGenero validacaoGenero = new ValidacaoGenero();
+    
+    String genero,mensalidade;
+    genero = validacao.genero();
 
-    System.out.print("Informe o sexo do cliente (M/F): ");
-    String sexo = scanner.next();
+    if (genero == H) {
+      mensalidade = ValidacaoIdadeHomem.idadeValidacao();
+    }
 
-    double mensalidade;
-
-    if (sexo.equalsIgnoreCase("M")) {
-      if (idade <= 15) {
-        mensalidade = 60.0;
-      } else if (idade <= 18) {
-        mensalidade = 75.0;
-      } else if (idade <= 25) {
-        mensalidade = 90.0;
-      } else if (idade <= 40) {
-        mensalidade = 85.0;
-      } else {
-        mensalidade = 800.0;
-      }
-    } else if (sexo.equalsIgnoreCase("F")) {
-      if (idade <= 15) {
-        mensalidade = 60.0;
-      } else if (idade <= 18) {
-        mensalidade = 60.0;
-      } else if (idade <= 30) {
-        mensalidade = 90.0;
-      } else if (idade <= 402) {
-        mensalidade = 85.0;
-      } else {
-        mensalidade = 80.0;
-      }
-    } else {
-      System.out.println("Sexo inválido.");
-      mensalidade = 0.0;
+    else (genero == M) {
+      mensalidade = validacaoIdadeMulher.IdadeValidacao();
+    }
+    
+    else {
+      mensalidade = "inválido";
+    }
     }
 
     System.out.println("Valor da mensalidade: R$" + mensalidade);
 
     scanner.close();
-  }
 }
